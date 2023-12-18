0cd92d298a6590cd4923093e274b3209924dc219
Sibansu <mishrasibansu16@gmail.com>
2023-12-18 12:48:38 +0530

    latest

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use("/api", userRoute);
 
 app.use(express.static(path.join(__dirname, './client/build')))
 
+
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
